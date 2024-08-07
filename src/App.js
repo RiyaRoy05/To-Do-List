a19d09e9ed24bf4fef62ae38965056169d06880e
tendercall <info@tendercall.com>
2024-08-07 23:00:20 +0530

    to-do-list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = () => {
+    if (input.trim() !== '') {
+      setTodos([...todos, input]);
+      setInput('');
+    }
+  };
+
+   const removeTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+   }
+
+   useEffect(() => {
+    const timer = setInterval(() => {
+      console.log('Timer tick');
+    }, 1000);
+  
+    return () => clearInterval(timer); 
+  }, []);
+  
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Simple To-Do List</h1>
+        <input 
+          type="text" 
+          value={input} 
+          onChange={(e) => setInput(e.target.value)} 
+          placeholder="Enter a new task" 
+        />
+        <button onClick={addTodo}>Add To-Do</button>
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>
+              {todo} <button onClick={() => removeTodo(index)}>Remove</button>
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
